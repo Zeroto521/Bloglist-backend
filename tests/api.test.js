6219946a5234ce9f0335c77e7cc180e1ee54bdfc
Zero <Zeroto521@gmail.com>
2020-12-16 16:11:15 +0800

    :tada: finish 4.10

--- tests/api.test.js
+++ tests/api.test.js
@@ -29,6 +29,28 @@ test('length of the blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12, __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(contents).toContain('Canonical string reduction')
+})
+
+
 afterAll(() => {
   mongoose.connection.close()
 })
